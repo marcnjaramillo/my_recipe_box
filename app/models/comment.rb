332814da8777c88cd64a9c3dbb7c3622ca4b95dc
marcnjaramillo <marcn.jaramillo@gmail.com>
2021-03-02 02:20:18 -0800

    Add models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,6 @@
 class Comment < ApplicationRecord
+  belongs_to :user
+  belongs_to :commentable, polymorphic: true
+  belongs_to :parent, optional: true, class_name: "Comment"
+  has_many :comments, foreign_key: :parent_id, dependent: :destroy
 end
