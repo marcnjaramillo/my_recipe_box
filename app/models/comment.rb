dfd9f6663c6421512ce66d600a02a08b1e1c6282
Marc Jaramillo <mnj.webdeveloper@gmail.com>
2021-03-04 16:27:59 -0800

    Update comment.rb

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,19 @@ class Comment < ApplicationRecord
   belongs_to :commentable, polymorphic: true
   belongs_to :parent, optional: true, class_name: "Comment"
   has_many :comments, foreign_key: :parent_id, dependent: :destroy
+  
+  validates :body, presence: true
+
+  after_create_commit do
+    broadcast_append_to [commentable, :comments], target: "#{dom_id(parent || commentable)}_comments", partial: "comments/comment_with_replies"
+  end
+
+  after_update_commit do
+    broadcast_replace_to self
+  end
+
+  after_destroy_commit do
+    broadcast_remove_to self
+    broadcast_action_to self, action: :remove, target: "#{dom_id(self)}_with_comments"
+  end
 end
