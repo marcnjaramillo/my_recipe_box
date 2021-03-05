e03efe7b35ff0a06247c460f669356837fff5dcd
marcnjaramillo <marcn.jaramillo@gmail.com>
2021-03-05 01:34:30 -0800

    Add associations to models

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,2 +1,7 @@
 class Ingredient < ApplicationRecord
+  has_many :recipe_ingredients
+  has_many :recipes, through: :recipe_ingredients, dependent: :destroy
+
+  validates :name, presence: true
+  validates :name, uniqueness: {case_sensitive: false}
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,12 @@
 class Recipe < ApplicationRecord
+  has_many :recipe_ingredients, inverse_of: :recipe, dependent: :destroy
+  has_many :ingredients, through: :recipe_ingredients
   has_many :comments, as: :commentable
+  has_one_attached :image
+
+  accepts_nested_attributes_for :recipe_ingredients, reject_if: :all_blank, allow_destroy: true
+
+  def self.categories
+    ['Breakfast', 'Lunch', 'Dinner', 'Dessert', 'Soup', 'Salad', 'Sauce', 'Appetizer', 'Side Dish']
+  end
 end
