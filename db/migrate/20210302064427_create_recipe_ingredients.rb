dbe84f63d940c14e0f5a5c8ffe455956785cf874
marcnjaramillo <marcn.jaramillo@gmail.com>
2021-03-19 01:47:11 -0700

    Redo migrations

--- db/migrate/20210302064427_create_recipe_ingredients.rb
+++ db/migrate/20210302064427_create_recipe_ingredients.rb
@@ -1,7 +1,7 @@
 class CreateRecipeIngredients < ActiveRecord::Migration[6.0]
   def change
     create_table :recipe_ingredients do |t|
-      t.string :quantity
+      t.string :quantity, null: false
       t.references :recipe
       t.references :ingredient
       t.timestamps
---
+++ db/migrate/20210302064430_create_directions.rb
@@ -0,0 +1,8 @@
+class CreateDirections < ActiveRecord::Migration[6.1]
+  def change
+    create_table :directions do |t|
+      t.text :body, null: false
+      t.timestamps
+    end
+  end
+end
