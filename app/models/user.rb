6a3b08f8bb16d50d8115c158223799386f512970
marcnjaramillo <marcn.jaramillo@gmail.com>
2021-03-19 01:49:42 -0700

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
-  has_many :recipes
+  
+  has_one_attached :profile_image
+  has_many :recipes, dependent: :destroy
   has_many :comments
+  has_many :followers, class_name: "Follower", foreign_key: "follower_id", dependent: :destroy
+  has_many :followings, through: :followers, source: :following
 
   def self.roles
     ['Admin', 'User']
+  end
+
+  def total_followers
+    Follower.where(follower_id: self.id).count
+  end
+
+  def total_following
+    Follower.where(following_id: self.id).count
   end
 end
