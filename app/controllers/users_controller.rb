149ede95b15b785d1a0d553ee57359fe96173b0c
marcnjaramillo <marcn.jaramillo@gmail.com>
2021-03-22 13:54:41 -0700

    Remove index view and action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,6 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
   before_action :set_user
 
-  # def index
-  #   following_ids = Follower.where(follower_id: current_user.id).map(&:following_id)
-  #   following_ids << current_user.id
-  #   @recipes = Recipe.includes(:user).where(user_id: following_ids)
-  #   @follower_suggestions = User.where.not(id: following_ids)
-  # end
 
   def profile
     @recipes = current_user.recipes
