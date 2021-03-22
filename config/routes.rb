a01252643fa1abf6cb2df7d6e63157ad68442fa0
marcnjaramillo <marcn.jaramillo@gmail.com>
2021-03-22 00:59:40 -0700

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,14 @@ Rails.application.routes.draw do
   root to: 'static#home'
   get "/dashboard" => "users#index"
   get "/profile/:username" => "users#profile", as: :profile
-  get "/followers" => "followers#index"
-  post "follow/user" => "users#follow_user", as: :follow_user
 
-  devise_for :users
+  devise_for :users, :path => 'account'
+
+  resources :users, param: :username do
+    member do
+      get :following, :followers
+    end
+  end
   
   resources :recipes do
     resources :comments, module: :recipes
@@ -14,5 +18,9 @@ Rails.application.routes.draw do
   resources :comments do
     resources :comments, module: :comments
   end
+
+  resources :relationships, only: [:create, :destroy]
+
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
