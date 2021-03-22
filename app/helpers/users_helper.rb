8054ead7cd54d95510b7bbc059d27e7324962ba0
marcnjaramillo <marcn.jaramillo@gmail.com>
2021-03-22 00:59:21 -0700

    Remove profile_image helper momentarily

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,8 @@
 module UsersHelper  
-  def profile_image user, width = 200
-    image = user.profile_image.present? ? user.profile_image : "default.png"
-    image_tag(image, width: width, class: "profile-pic m-r-10")
-  end
+  # def profile_image user, width = 200
+  #   image = user.profile_image.present? ? user.profile_image : "default.png"
+  #   image_tag(image, width: width, class: "profile-pic m-r-10")
+  # end
 
   def can_edit_profile? profile_id
     user_signed_in? && current_user.id == profile_id
