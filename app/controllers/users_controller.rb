a94609ff1e21fc17b58082f0fb2a194f51ad980b
marcnjaramillo <mnj.webdeveloper@gmail.com>
2021-07-22 15:19:44 -0700

    Begin working on new feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,13 +10,13 @@ class UsersController < ApplicationController
   def following
     @title = "Following"
     @users = @user.following
-    render 'show_follow'
+    render 'following'
   end
 
   def followers
     @title = "Followers"
     @users = @user.followers
-    render 'show_follow'
+    render 'followers'
   end
 
   private
