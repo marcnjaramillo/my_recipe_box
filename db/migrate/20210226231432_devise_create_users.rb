b330f16fc0e63030fc7fd20e72dfd478938a8a44
Marc Jaramillo <mnj.webdeveloper@gmail.com>
2022-01-25 16:11:56 -0800

    Clean up user model

--- db/migrate/20210226231432_devise_create_users.rb
+++ db/migrate/20210226231432_devise_create_users.rb
@@ -9,9 +9,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :username, null: false, unique: true
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-      t.string :role, null: false, default: "User"
-      t.integer :followers_count, default: 0
-      t.integer :following_count, default: 0
+      t.boolean :admin, null: false, default: false
       
       ## Recoverable
       t.string   :reset_password_token
--- db/schema.rb
+++ db/schema.rb
@@ -118,9 +118,7 @@ ActiveRecord::Schema.define(version: 2021_03_02_093925) do
     t.string "username", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "role", default: "User", null: false
-    t.integer "followers_count", default: 0
-    t.integer "following_count", default: 0
+    t.boolean "admin", default: false, null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
