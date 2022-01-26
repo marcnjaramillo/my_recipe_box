5d75e304cf61a191fbe24f0186bd4440263978df
Marc Jaramillo <mnj.webdeveloper@gmail.com>
2022-01-25 16:13:20 -0800

    Remove roles and clean up spacing

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,14 +7,13 @@ class User < ApplicationRecord
   has_one_attached :profile_image
   has_many :recipes, dependent: :destroy
   has_many :comments
-  has_many :active_relationships, class_name:  "Relationship", foreign_key: "follower_id", dependent:   :destroy
-  has_many :passive_relationships, class_name:  "Relationship", foreign_key: "followed_id", dependent:   :destroy
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :following, through: :active_relationships,  source: :followed
-  has_many :followers, through: :passive_relationships, source: :follower
+  has_many :followers, through: :passive_relationships
 
-  def self.roles
-    ['Admin', 'User']
-  end
+  validates_presence_of :first_name, :last_name, :username, :admin
+  validates_uniqueness_of :username, :email
 
   def fullname
     "#{first_name} #{last_name}"
